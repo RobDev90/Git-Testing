b176934f9f84d8480f786a3bd88f2bc81fd0cb53
Rob Parr <rob@kingslandlinassi.com>
2017-11-02 12:13:35 +0000

    Initial commit.

---
+++ header.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<head>
+    <title>Title</title>
+    <link rel="stylesheet" type="text/css" media="all" href="css/base.css"/>
+</head>
+
+<div class="container">
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    require_once('header.php');
+?>
+
+<body>
+    <article>
+        <h1>Heading 1<h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus ex, doloribus! Perspiciatis repellendus ad odit soluta voluptatem similique consequuntur, eveniet earum nisi qui consectetur totam officia voluptates perferendis voluptatibus aut.</p>
+    </article>
+</body>
+
+<?php 
+    require_once('footer.php');
+?>
