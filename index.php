5a2321fea45e00708bddff3bd52362cd1b689c1d
Rob Parr <rob@kingslandlinassi.com>
2017-11-02 15:36:04 +0000

    new functionality added

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <article>
         <h1>Heading 1<h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus ex, doloribus! Perspiciatis repellendus ad odit soluta voluptatem similique consequuntur, eveniet earum nisi qui consectetur totam officia voluptates perferendis voluptatibus aut.</p>
+        <p>NEW FUNCTIONALITY THAT GETS REJECTED BY CLIENT</p>
     </article>
 </body>
 
