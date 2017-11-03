016837db0474da6aaecf98b646e8607f1ea4c4b9
Rob Parr <rob@kingslandlinassi.com>
2017-11-03 16:44:56 +0000

    Added newsletter function

--- index.php
+++ index.php
@@ -7,6 +7,10 @@
         <h1>Heading 1<h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus ex, doloribus! Perspiciatis repellendus ad odit soluta voluptatem similique consequuntur, eveniet earum nisi qui consectetur totam officia voluptates perferendis voluptatibus aut.</p>
     </article>
+    <article class="new-branch-newsletter">
+        <h2>Newsletter Signup</h2>
+        <p>Signup to newsletter here.</p>
+    </article>
 </body>
 
 <?php 
